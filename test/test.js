d72cfdbc529910f1a30c64177d1ede972de2ef2d
Dan Barela <daniel.barela@gmail.com>
2015-12-01 13:30:07 -0700

    Adding in tests for ormlite-jdbc and apache lucene

--- test/test.js
+++ test/test.js
@@ -1,19 +1,33 @@
-var java = require('java')
+var fs = require('fs-extra')
+  , path = require('path')
   , mvn = require('../index');
 
 describe('maven', function() {
+  var localRepository = path.join(__dirname, '.m2');
+  console.log('local repo', localRepository);
+  beforeEach(function(done) {
+    fs.remove(localRepository, done);
+  });
+  afterEach(function(done) {
+    fs.remove(localRepository, done);
+  });
   it('should pull the maven dependencies for ormlite-jdbc 4.48', function(done) {
-    mvn({debug: true, packageJsonPath: __dirname+'/ormlite-package.json'}, function(err, mvnResults) {
+    this.timeout(15000);
+    mvn({localRepository: localRepository, packageJsonPath: __dirname+'/ormlite-jdbc-package.json'}, function(err, mvnResults) {
       if (err) {
         done(err);
         return console.error('could not resolve maven dependencies', err);
       }
-      mvnResults.classpath.forEach(function(c) {
-        console.log('adding ' + c + ' to classpath');
-        java.classpath.push(c);
-      });
-      self.initialized = true;
-      console.log('done');
+      done();
+    });
+  });
+  it('should pull the maven dependencies for lucene-core 4.9.0', function(done) {
+    this.timeout(15000);
+    mvn({localRepository: localRepository, packageJsonPath: __dirname+'/lucene-core-package.json'}, function(err, mvnResults) {
+      if (err) {
+        done(err);
+        return console.error('could not resolve maven dependencies', err);
+      }
       done();
     });
   });
